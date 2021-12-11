c9412fa7d4a10bfe0d3c7ca22fddfcbd9f2ffb73
KalebCode <65588397+KalebCode@users.noreply.github.com>
2021-12-10 23:21:25 -0300

    Add files via upload

--- projetoReact/App.js
+++ projetoReact/App.js
@@ -1,1 +1,151 @@
+/* Trabalho de PAM parte 2
+ * 
+ * Autor: Kaleb Ribeiro - 2 DS A 
+ * ...............................
+*/
 
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+
+import
+ MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {
+  NavigationContainer
+} from '@react-navigation/native';
+import {
+  createStackNavigator
+} from '@react-navigation/stack';
+import {
+  createBottomTabNavigator
+} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
+import SettingsScreen from './SettingsScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#000' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'bold' }
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Página Home' }}/>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ title: 'Details Page' }} />
+        <Stack.Screen
+          name="Lista de Páginas"
+          component={SettingsScreen}
+          options={{ title: 'Página de Detalhes' }} />
+      </Stack.Navigator>
+  );
+}
+
+function SettingsStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Settings"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#000' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' }
+      }}>
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ title: 'Página de Listagem' }}/>
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{ title: 'Página de Detalhes' }}/>
+    </Stack.Navigator>
+  );
+}
+
+
+function DetailsStack() {
+    return (
+      <Stack.Navigator
+        initialRouteName="Details"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#000' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'bold' }
+        }}>
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ title: 'Página de Listagem' }}/>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ title: 'Página de Detalhes' }}/>
+      </Stack.Navigator>
+    );
+  }
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        tabBarOptions={{
+          activeTintColor: '#000',
+        }}>
+        <Tab.Screen
+          name="Sobre"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Sobre',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="home"
+                color={color}
+                size={size}
+              />
+            ),
+          }}  />
+        <Tab.Screen
+          name="Lista de páginas"
+          component={SettingsStack}
+          options={{
+            tabBarLabel: 'Lista de Páginas',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="paglist"
+                color={color}
+                size={size}
+              />
+            ),
+          }} />
+        <Tab.Screen
+          name="Detalhes"
+          component={DetailsStack}
+          options={{
+            tabBarLabel: 'Detalhes',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="settings"
+                color={color}
+                size={size}
+              />
+            ),
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
---
+++ projetoReact/SettingsScreen.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+const SettingsScreen = ({ route, navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16
+            }}>
+            Esta página lista todas as outras
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate('Home')
+            }>
+            <Text>Voltar para o Início</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate('Details')
+            }>
+            <Text>Abrir Página de Detalhes</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 300,
+    marginTop: 16,
+  },
+});
+export default SettingsScreen;
